c9b14ae17882e19f2a166a0024c5915c62e6f0e0
ximemolina <ximenamolinaportilla01@gmail.com>
2026-05-03 19:01:42 -0600

    teletransporte minijuego 1
    en efecto, aun no logro q teletransporte bien

--- Corazon/Assets/Scripts/Teletransporte.cs
+++ Corazon/Assets/Scripts/Teletransporte.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
-
 public class Teletransportacion : MonoBehaviour
 {
     public Transform Target;
     public GameObject ThePlayer;
-
     private void OnTriggerEnter(Collider other)
     {
-        ThePlayer.transform.position= Target.transform.position;
+        if (other.gameObject == ThePlayer)
+        {
+            ThePlayer.transform.position = Target.transform.position;
+        }
     }
 }
